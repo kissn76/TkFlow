6118113433634aa5574d1ef12262b71973404c8e
fff <kiss.norbert@contrall.hu>
2023-09-04 16:01:33 +0200

    dev

--- mainwindow.py
+++ mainwindow.py
@@ -10,8 +10,8 @@ class Mainwindow(tk.Tk):
         super().__init__()
         self.protocol("WM_DELETE_WINDOW", self.quit)
 
-        self.widget_width = 200
-        self.widget_height = 100
+        self.widget_width_min = 200
+        self.widget_height_min = 100
         self.resize_width = 6
         self.widget_counter = 0
 
@@ -76,34 +76,55 @@ class Mainwindow(tk.Tk):
 
 
     def widget_create(self, module, x=10, y=10):
-        module_name = f"{module}.{self.widget_counter}"
+        widget_name = f"{module}.{self.widget_counter}"
 
         id_move = self.can_main.create_image(x, y, image=self.image_move, anchor="nw")
-        self.can_main.addtag_withtag(f"{module_name}.move", id_move)
+        self.can_main.addtag_withtag(f"{widget_name}.move", id_move)
+        move_box = self.can_main.bbox(f"{widget_name}.move")
 
-        id_widget_name = self.can_main.create_text(x, y, text=module_name, anchor="nw")
-        self.can_main.addtag_withtag(f"{module_name}.widget_name", id_widget_name)
+        id_widget_name = self.can_main.create_text(move_box[2], move_box[1], text=widget_name, anchor="nw")
+        self.can_main.addtag_withtag(f"{widget_name}.widget_name", id_widget_name)
 
         module = self.modules.new_object(module, self.can_main)
-        id_module = self.can_main.create_window(x, y, window=module, anchor="nw", width=self.widget_width)
-        self.can_main.addtag_withtag(f"{module_name}.module", id_module)
+        id_module = self.can_main.create_window(move_box[0], move_box[3], window=module, anchor="nw", width=self.widget_width_min)
+        self.can_main.addtag_withtag(f"{widget_name}.module", id_module)
+        module_box = self.can_main.bbox(f"{widget_name}.module")
 
-        id_background = self.can_main.create_rectangle(x, y, x, y, fill='red', outline='red')
-        self.can_main.addtag_withtag(f"{module_name}.background", id_background)
+        background_box_width = module_box[2] - 1 - move_box[0] - 1
+        background_box_height = module_box[3] - 1 - move_box[1] - 1
+        if background_box_width < self.widget_width_min:
+            background_box_width = self.widget_width_min
+        if background_box_height < self.widget_height_min:
+            background_box_height = self.widget_height_min
+        id_background = self.can_main.create_rectangle(
+                move_box[0],
+                move_box[1],
+                move_box[0] + background_box_width,
+                move_box[1] + background_box_height,
+                fill='red',
+                outline='red'
+            )
+        self.can_main.addtag_withtag(f"{widget_name}.background", id_background)
         self.can_main.tag_lower(id_background, id_move)
+        background_box = self.can_main.bbox(f"{widget_name}.background")
 
-        id_resize_w = self.can_main.create_line(x, y, x, y, width=self.resize_width)
-        self.can_main.addtag_withtag(f"{module_name}.resize_w", id_resize_w)
+        id_resize_w = self.can_main.create_line(background_box[2], background_box[1] + 1, background_box[2], background_box[3], width=self.resize_width)
+        self.can_main.addtag_withtag(f"{widget_name}.resize_w", id_resize_w)
 
-        id_resize_h = self.can_main.create_line(x, y, x, y, width=self.resize_width)
-        self.can_main.addtag_withtag(f"{module_name}.resize_h", id_resize_h)
+        id_resize_h = self.can_main.create_line(background_box[0] + 1, background_box[3], background_box[2], background_box[3], width=self.resize_width)
+        self.can_main.addtag_withtag(f"{widget_name}.resize_h", id_resize_h)
 
-        id_resize_wh = self.can_main.create_rectangle(x, y, x + self.resize_width, y + self.resize_width, fill='black', outline='yellow')
-        self.can_main.addtag_withtag(f"{module_name}.resize_wh", id_resize_wh)
+        id_resize_wh = self.can_main.create_rectangle(
+                background_box[2] - self.resize_width,
+                background_box[3] - self.resize_width,
+                background_box[2] + self.resize_width,
+                background_box[3] + self.resize_width,
+                fill='black',
+                outline='yellow'
+            )
+        self.can_main.addtag_withtag(f"{widget_name}.resize_wh", id_resize_wh)
 
         self.widget_counter += 1
-
-        self.widget_move(module_name, x, y)
 
 
     # DRAG & DROP metódusok
@@ -117,20 +138,58 @@ class Mainwindow(tk.Tk):
         tags = self.can_main.gettags('selected')
         if len(tags) > 0:
             module_name, module_counter, widget_function = tags[0].split('.')
+            mouse_x, mouse_y = event.x, event.y
             if widget_function == "move":
-                mouse_x, mouse_y = event.x, event.y
                 self.widget_move(f"{module_name}.{module_counter}", mouse_x, mouse_y)
             elif widget_function == "resize_w":
-                print("resize w")
+                self.widget_resize_width(f"{module_name}.{module_counter}", mouse_x)
             elif widget_function == "resize_h":
-                print("resize h")
+                self.widget_resize_height(f"{module_name}.{module_counter}", mouse_y)
             elif widget_function == "resize_wh":
-                print("resize w h")
+                self.widget_resize_width(f"{module_name}.{module_counter}", mouse_x)
+                self.widget_resize_height(f"{module_name}.{module_counter}", mouse_y)
 
 
     def widget_dnd_deselect(self, event):
         self.can_main.dtag('selected')    # removes the 'selected' tag
         self.can_main.unbind('<Motion>')
+
+
+    def widget_resize_width(self, widget_name, x):
+        canvas_x = self.can_main.canvasx(x)
+        if x > self.can_main.winfo_width():
+            self.can_main.xview_scroll(1, 'units')
+        if x < 1:
+            self.can_main.xview_scroll(-1, 'units')
+
+        move_box = self.can_main.bbox(f"{widget_name}.move")
+        width = canvas_x - move_box[0]
+        if width >= self.widget_width_min:
+            background_box = self.can_main.bbox(f"{widget_name}.background")
+            self.can_main.coords(f"{widget_name}.background", move_box[0], move_box[1], canvas_x, background_box[3] - 1)
+            background_box = self.can_main.bbox(f"{widget_name}.background")
+            self.can_main.itemconfigure(f"{widget_name}.module", width=width)
+            self.can_main.coords(f"{widget_name}.resize_w", background_box[2], background_box[1] + 1, background_box[2], background_box[3])
+            self.can_main.coords(f"{widget_name}.resize_h", background_box[0] + 1, background_box[3], background_box[2], background_box[3])
+            self.can_main.coords(f"{widget_name}.resize_wh", background_box[2] - self.resize_width, background_box[3] - self.resize_width, background_box[2] + self.resize_width, background_box[3] + self.resize_width)
+
+
+    def widget_resize_height(self, widget_name, y):
+        canvas_y = self.can_main.canvasy(y)
+        if y > self.winfo_height():
+            self.can_main.yview_scroll(1, 'units')
+        if y < 1:
+            self.can_main.yview_scroll(-1, 'units')
+
+        move_box = self.can_main.bbox(f"{widget_name}.move")
+        height = canvas_y - move_box[1]
+        if height >= self.widget_height_min:
+            background_box = self.can_main.bbox(f"{widget_name}.background")
+            self.can_main.coords(f"{widget_name}.background", move_box[0], move_box[1], background_box[2] - 1, canvas_y)
+            background_box = self.can_main.bbox(f"{widget_name}.background")
+            self.can_main.coords(f"{widget_name}.resize_w", background_box[2], background_box[1] + 1, background_box[2], background_box[3])
+            self.can_main.coords(f"{widget_name}.resize_h", background_box[0] + 1, background_box[3], background_box[2], background_box[3])
+            self.can_main.coords(f"{widget_name}.resize_wh", background_box[2] - self.resize_width, background_box[3] - self.resize_width, background_box[2] + self.resize_width, background_box[3] + self.resize_width)
 
 
     def widget_move(self, widget_name, x, y):
@@ -147,14 +206,27 @@ class Mainwindow(tk.Tk):
 
         self.can_main.coords(f"{widget_name}.move", canvas_x, canvas_y)
         move_box = self.can_main.bbox(f"{widget_name}.move")
+
         self.can_main.coords(f"{widget_name}.widget_name", move_box[2], move_box[1])
         self.can_main.coords(f"{widget_name}.module", move_box[0], move_box[3])
-        module_box = self.can_main.bbox(f"{widget_name}.module")
-        self.can_main.coords(f"{widget_name}.background", move_box[0], move_box[1], module_box[2], module_box[3])
+
         background_box = self.can_main.bbox(f"{widget_name}.background")
-        self.can_main.coords(f"{widget_name}.resize_w", background_box[2], background_box[1], background_box[2], background_box[3])
-        self.can_main.coords(f"{widget_name}.resize_h", background_box[0], background_box[3], background_box[2], background_box[3])
-        self.can_main.coords(f"{widget_name}.resize_wh", background_box[2] - self.resize_width / 2, background_box[3] - self.resize_width / 2, background_box[2] + self.resize_width / 2, background_box[3] + self.resize_width / 2)
+        background_box_width = background_box[2] - 1 - background_box[0] - 1
+        background_box_height = background_box[3] - 1 - background_box[1] - 1
+        self.can_main.coords(
+                f"{widget_name}.background",
+                move_box[0],
+                move_box[1],
+                move_box[0] + background_box_width,
+                move_box[1] + background_box_height
+            )
+        background_box = self.can_main.bbox(f"{widget_name}.background")
+
+        self.can_main.coords(f"{widget_name}.resize_w", background_box[2], background_box[1] + 1, background_box[2], background_box[3])
+
+        self.can_main.coords(f"{widget_name}.resize_h", background_box[0] + 1, background_box[3], background_box[2], background_box[3])
+
+        self.can_main.coords(f"{widget_name}.resize_wh", background_box[2] - self.resize_width, background_box[3] - self.resize_width, background_box[2] + self.resize_width, background_box[3] + self.resize_width)
     # DRAG & DROP metódusok END
 
 
